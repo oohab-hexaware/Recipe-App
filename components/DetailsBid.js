d0eb348920db26461708128ab23025a9e2bab157
oohab-hexaware <115135348+oohab-hexaware@users.noreply.github.com>
2022-10-20 15:39:27 +0530

    Add files via upload

---
+++ components/DetailsBid.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { COLORS, SIZES, FONTS } from "../constants";
+
+const DetailsBid = ({ bid }) => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginVertical: SIZES.base,
+        paddingHorizontal: SIZES.base,
+      }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          paddingHorizontal: SIZES.base,
+        }}>
+        <Text
+          style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: SIZES.small,
+            color: COLORS.primary,
+          }}>
+          Bid placed by {bid.name}
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small - 2,
+            color: COLORS.secondary,
+            marginTop: 3,
+          }}>
+          {bid.date}
+        </Text>
+      </View>
+    </View>
+  );
+};
+export default DetailsBid;
---
+++ components/NFTCard.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { View, Image } from "react-native";
+
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
+import { RectButton, CircleButton } from "./Button";
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View
+        style={{
+          width: "100%",
+          height: 300,
+        }}
+      >
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "110%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
+
+      </View>
+
+      <SubInfo />
+
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={150}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default NFTCard;
