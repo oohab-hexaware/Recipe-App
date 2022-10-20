6078bf17676841184093d38e8f03c6301f30876b
oohab-hexaware <115135348+oohab-hexaware@users.noreply.github.com>
2022-10-20 15:40:43 +0530

    Add files via upload

---
+++ constants/dummy.js
@@ -0,0 +1,71 @@
+import assets from "./assets";
+
+const NFTData = [
+  {
+    id: "NFT-01",
+    name: "Botanist Gin Cocktail",
+    creator: "Islay, Scotland",
+    
+    description:
+    "Fill a shaker with ice cubes.Add 1½ ounce Beehive Distillery Jack Rabbit Gin,½ ounce St-Germain liqueur,½ ounce lemon juice,¼ ounce simple syrup, 2 cucumber slices,Mint sprig and shake .Then strain into a rocks glass with 3 large ice cubes. Garnish with a cucumber slice and mint sprig.",
+    image: assets.nft01,
+  },
+  {
+    id: "NFT-02",
+    name: "Blue Lagoon Cocktail",
+    creator: "Australia",
+   
+    description:
+      "Fill a shaker with ice. Then add 1 1/2 oz Vodka and 1 1/2 oz Blue Curacao and shake. Strain into an ice filled glass, top with 6 oz lemonade, garnish.",
+    image: assets.nft02,
+  },
+  {
+    id: "NFT-03",
+    name: "Sunrise Mocktail",
+    creator: "Sausalito, California",
+  
+    description:
+      "In a champagne glass, or glass of your choice, pour equal amounts orange juice and your choice of sparkling drink.Take the grenadine and slowly pour it into the glass,Grenadine is very sweet, so you don't need much.Garnish with strawberries or cherries or any fruits of your choice.",
+    image: assets.nft03,
+  },
+  {
+    id: "NFT-04",
+    name: "Sip Of Pride",
+    creator: "London",
+    
+    description:
+    "Take 100g Blackberries for Dark Blue / Purple Layer,50g Green Apple and 50g Kiwi for Green Layer,100g Pineapple for Yellow Layer,50g Mango and 50g Peach for Orange Layer,100g Strawberries for Red Layer.Blend each layer with 200ml Chardonnay, 1 scoop of crushed ice and frozen fruit.Layer in glass jug, freezing in between each layer to help the separation hold.Garnish with fresh fruit.",
+    description:
+    "Take 100g Blackbe",
+    image: assets.nft04,
+  },
+  {
+    id: "NFT-05",
+    name: "Icy-Cold Mojito",
+    creator: "Cuba",
+    
+    description:
+      "In cocktail shaker, with wooden spoon, crush mint, lime juice, and sugar.Add ice cubes and golden rum. Shake until well blended.Fill 1 tall glass with ice cubes. Strain drink into glass and top off with seltzer. Garnish with mint sprig and lime wedge.",
+    image: assets.nft05,
+  },
+  {
+    id: "NFT-06",
+    name: "Zombie Cocktail",
+    creator: "New York",
+    
+    description:
+      "Take 1 ounce white rum,,1 ounce golden rum1 ounce dark rum,1 ounce apricot brand,1 ounce pineapple juice,1 ounce papaya juice ,Dash of grenadine.Shake all the ingredients in a shaker.Pour the mixture into a glass.Pour the 151-proof rum on the top of the drink.You can definitely light your Zombie on fire at this point if you want.Garnish with a cherry, a slice of pineapple, an orange slice, all of the above, or pretty much whatever fruit you feel like.",
+    image: assets.nft06,
+  },
+  {
+    id: "NFT-07",
+    name: "Vanilla Almond Cold Brew",
+    creator: "Madagascar",
+    
+    description:
+      "Grind the coffee beans to a medium-coarse consistency.In a French press or large mason jar, combine the coffee grinds and water. Stir and top with the lid. Steep at room temperature or in the fridge for 12 - 24 hours. The longer it sits, the stronger the coffee taste.To make the vanilla cream, combine the cream, soy milk, vanilla, and agave syrup in a separate jar, and store in the fridge until ready to use.he next day, place a strainer over a bowl lined with cheese cloth (or nut milk bag). Strain the grounds through the cheesecloth. If desired, filter the coffee through a second time to remove any residual ground beans. The liquid that remains in the bowl is now your cold brew coffee concentrate!To make the drink, fill a glass with ice. Then pour in 2 parts cold brew concentrate, 1 part cold water, and 1 part vanilla cream. ",
+    image: assets.nft07,
+  },
+];
+
+export { NFTData };
